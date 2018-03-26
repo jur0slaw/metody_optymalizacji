6cbc56306eae0345daf6629536f6e79648013e8f
student <student>
2018-03-26 14:45:26 +0200

    robert

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
+import matplotlib
+matplotlib.use('module://kivy.garden.matplotlib.backend_kivy')
+import matplotlib.pyplot as plt
 from kivy.app import App
+from kivy.garden.matplotlib.backend_kivy import FigureCanvasKivy
+import random
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import Color
+import numpy as np
 
 
-class Andrzej(BoxLayout):
-    pass
 
-class MetUI(Widget):
+
+
+
+class PlotClass(BoxLayout):
+    plt.plot([1, 23, 2, 4])
+    plt.ylabel('some numbers')
+    self.add_widget(FigureCanvasKivy(plt.gcf()))
+
+class MetUI(BoxLayout):
     pass
 
 
 class MetApp(App):
     def build(self):
-        return MetUI(Andrzej)
+        return MetUI()
 
 
 
